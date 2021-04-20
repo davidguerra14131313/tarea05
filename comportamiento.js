c16fed78a0d68c16420af0fc5df9c9e38ba54673
david <contac@faztweb.com>
2021-04-20 02:30:26 -0500

    lectura de combobox

--- comportamiento.js
+++ comportamiento.js
@@ -1,59 +1,44 @@
 let txtNombre=document.querySelector('#producto')
 let botonBuscar=document.querySelector('#botonBuscar')
-
 let contenidoTabla =document.querySelector('#contenidoTabla')
 let comboCategoria=document.querySelector('#categoria')
 
-/*
-const buscarcategoria =()=>
-{event.preventDefault();
+
+
+
     
   
-    const uricategoria ='https://disenoydesarrolloapi.azurewebsites.net/api/Producto/categorias'
+    const uricategoria ='https://disenoydesarrolloapi.azurewebsites.net/api/Producto/Categorias'
     fetch(uricategoria)
     .then(response=>response.json())
     .then(datacombo=>
-        imprimircombo(datacombo))
-
-}
-
-const imprimircombo=(combito)=>
-{  comboCategoria.innerHTML=""
-    combito.categorias.forEach(elementof => {
-        comboCategoria.innerHTML+="<option>"+elementof+"<option>"
-    });
- 
+        {
+            datacombo.categorias.forEach(elementOf=>
+            {
+                comboCategoria.innerHTML+='<option>'+elementOf+'</option>'
+            })
+  
+    })
     
-
-}comboCategoria.addEventListener('load   ',buscarcategoria);
-
-
-*/
-
-
-
-
-
-
 
 const buscardato=()=>
 {
     event.preventDefault();
-    
+    const combo=comboCategoria.value;
     const nombre=txtNombre.value;
-    const uri='https://disenoydesarrolloapi.azurewebsites.net/api/Producto?Nombre='
-    
+    const uri='https://disenoydesarrolloapi.azurewebsites.net/api/Producto'
+    const uriconcatenado= uri+'?Nombre='+nombre+'&Categoria='+combo
 
-    fetch(uri+nombre)
+    fetch(uriconcatenado)
     .then(response=>response.json())
     .then(data=>
         imprimirdato(data))
 }
-const imprimirdato=(datasa)=>
+const imprimirdato=(data)=>
 {
     
     let contenido='';
-    datasa.forEach(elementOf => {
+    data.forEach(elementOf => {
        contenido+=
                   '</td><td>'+elementOf.id+
                   '</td><td>'+elementOf.nombre+
@@ -64,4 +49,4 @@ const imprimirdato=(datasa)=>
     });
     contenidoTabla.innerHTML=contenido
 }
-botonBuscar.addEventListener('click',buscardato);
+botonBuscar.addEventListener('click',buscardato)
